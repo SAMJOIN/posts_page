a5e948fa21183ff8283c3af3adce1211be54469a
SAMJOIN <nikita.korneev.2222@mail.ru>
2024-03-10 22:52:34 +0300

    V2

--- src/Paginator/Paginator.tsx
+++ src/Paginator/Paginator.tsx
@@ -3,50 +3,33 @@ import style from './Paginator.module.css'
 
 const Paginator = (props: PaginatorProps) => {
 
+
     let pages: number[] = [];
-    let visiblePages: number[] = [];
     const pageCount = props.pageCount;
     const currentPage = props.currentPage;
-
-    if (pageCount > 5) {
-        if (currentPage - 4 < 3) {
-
-            for (let i = 2; i < currentPage + 5; i++) {
-                visiblePages.push(i);
-            }
-            // visiblePages.push('...');
-        }
-        else if (currentPage + 4 > pageCount) {
-            // visiblePages.push('...');
-            for (let i = currentPage - 4; i < pageCount; i++) {
-                visiblePages.push(i);
-            }
-        }
-        else if (currentPage - 4 > 2 || currentPage + 4 < pageCount) {
-            // visiblePages.push('...');
-            for (let i = currentPage - 4; i < currentPage + 5; i++)
-                visiblePages.push(i);
-            // visiblePages.push('...');
-        }
-        pages = [1, ...visiblePages, pageCount];
-    } else {
-        for (let i = 1; i <= pageCount; i++) {
-            pages.push(i);
-        }
-    }
+    for (let i = 1; i <= pageCount; i++)
+        pages.push(i);
 
     return (
+        // <div className={style.paginatorContainer}>
+        //     {pages.map(page => {
+        //         if (page === currentPage) {
+        //             return <span key={page} className={currentPage === page ? style.selectedPage : style.pageNumber} >{'   ' + page + '   '}</span>
+        //         } else {
+        //             return <span key={page} className={currentPage === page ? style.selectedPage : style.pageNumber} onClick={() => props.onPageChange(page)}>{'   ' + page + '   '}</span>
+        //         }
+        //     })}
+        // </div>
         <div className={style.paginatorContainer}>
+            <button className={style.button} disabled={currentPage === 1} onClick={() => currentPage > 1 && props.onPageChange(currentPage - 1)}>{'<'}</button>
             {pages.map(page => {
                 if (page === currentPage) {
                     return <span key={page} className={currentPage === page ? style.selectedPage : style.pageNumber} >{'   ' + page + '   '}</span>
-                // }
-                // else if (page === '...') {
-                    // return <span key={page}>{'   ' + page + '   '}</span>
                 } else {
                     return <span key={page} className={currentPage === page ? style.selectedPage : style.pageNumber} onClick={() => props.onPageChange(page)}>{'   ' + page + '   '}</span>
                 }
             })}
+            <button className={style.button} disabled={currentPage === pageCount} onClick={() => currentPage < pageCount && props.onPageChange(currentPage + 1)}>{'>'}</button>
         </div>
     );
 }
